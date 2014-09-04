13d1df42be1c0e743467d6b194a8571e75034816
root <root@dev-joh-01.orthobanc.local>
2014-09-04 09:11:47 -0400

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,73 @@
+class chocolatey_packages {
+
+  $provider	= 'chocolatey'
+
+  exec { "install-chocolatey":
+    creates	=> 'C:\ProgramData\chocolatey',
+    command     => 'C:\windows\system32\cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"',
+  }
+
+  package { 'GoogleChrome':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> Exec['install-chocolatey'],
+  }
+
+  file { 'C:\users\public\desktop\Google Chrome.lnk':
+    ensure	=> absent,
+    subscribe	=> Package['GoogleChrome'],
+  }
+
+  package { 'Firefox':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> File['C:\users\public\desktop\Google Chrome.lnk'],
+  }
+
+  file { 'C:\users\public\desktop\Mozilla Firefox.lnk':
+    ensure	=> absent,
+    subscribe	=> Package['Firefox'],
+  }
+
+  package { 'flashplayeractivex':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> File['C:\users\public\desktop\Mozilla Firefox.lnk'],
+  }
+
+  package { 'flashplayerplugin':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> Package['flashplayeractivex'],
+  }
+
+  package { 'adobereader':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> Package['flashplayerplugin'],
+  }
+
+  file { 'C:\users\public\desktop\Adobe Reader XI.lnk':
+    ensure	=> absent,
+    subscribe	=> Package['adobereader'],
+  }
+
+  package { 'javaruntime':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> File['C:\users\public\desktop\Adobe Reader XI.lnk'],
+  }
+
+  package { '7zip':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> Package['javaruntime'],
+  }
+
+  package { 'iTunes':
+    ensure      => installed,
+    provider    => $provider,
+    require	=> Package['7zip'],
+  }
+
+}
