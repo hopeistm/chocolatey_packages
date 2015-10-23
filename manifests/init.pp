8292897071cbb4325c292288aa6f0d87e3a418c3
Joshua HOPE <joshua.simon@operationhope.org>
2015-10-22 18:41:40 -0700

    if os statement confirmed

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,7 +21,7 @@
 #
 class chocolatey_packages {
 
-  if ($::operatingsystemmajrelease != '7')
+  if ($::operatingsystemmajrelease != '8')
   {
     err('This module works on Windows 8 only!')
     fail('Unsupported OS')
