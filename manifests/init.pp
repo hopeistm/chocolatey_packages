2f4eea03c4048dca9dbfe4ccade905ba6378dd89
Joshua HOPE <joshua.simon@operationhope.org>
2015-10-22 18:43:57 -0700

    prevent error from stopping puppet run

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,80 +21,65 @@
 #
 class chocolatey_packages {
 
-  if ($::operatingsystemmajrelease != '8')
-  {
-    err('This module works on Windows 8 only!')
-    fail('Unsupported OS')
-  }
-
   $provider	= 'chocolatey'
 
-  # exec { "install-chocolatey":
-  #   creates	=> 'C:\ProgramData\chocolatey',
-  #   command     => 'C:\windows\system32\cmd.exe /K @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString(\'https://chocolatey.org/install.ps1\'))"',
-  # }
+  if ($::operatingsystemmajrelease = '8')
+  {
+      package { 'GoogleChrome':
+        ensure      => latest,
+        provider    => $provider,
+        # require	=> Exec['install-chocolatey'],
+      }
 
-  package { 'GoogleChrome':
-    ensure      => latest,
-    provider    => $provider,
-    # require	=> Exec['install-chocolatey'],
+      # file { 'C:\users\public\desktop\Google Chrome.lnk':
+      #   ensure	=> absent,
+      #   subscribe	=> Package['GoogleChrome'],
+      # }
+
+      package { 'Firefox':
+        ensure      => latest,
+        provider    => $provider,
+        # require	=> File['C:\users\public\desktop\Google Chrome.lnk'],
+      }
+
+      file { 'C:\users\public\desktop\Mozilla Firefox.lnk':
+        ensure	=> absent,
+        subscribe	=> Package['Firefox'],
+      }
+
+      # package { 'flashplayeractivex':
+      #   ensure      => installed,
+      #   provider    => $provider,
+      #   require	=> File['C:\users\public\desktop\Mozilla Firefox.lnk'],
+      # }
+
+      # package { 'flashplayerplugin':
+      #   ensure      => latest,
+      #   provider    => $provider,
+      #   require	=> Package['flashplayeractivex'],
+      # }
+
+      package { 'adobereader':
+        ensure      => latest,
+        provider    => $provider,
+        # require	=> Package['flashplayerplugin'],
+      }
+
+      file { 'C:\users\public\desktop\Adobe Reader DC.lnk':
+        ensure	=> absent,
+        subscribe	=> Package['adobereader'],
+      }
+
+      # package { 'javaruntime':
+      #   ensure      => latest,
+      #   provider    => $provider,
+      #   require	=> File['C:\users\public\desktop\Adobe Reader XI.lnk'],
+      # }
+
+      package { '7zip':
+        ensure      => latest,
+        provider    => $provider,
+      }
   }
-
-  # file { 'C:\users\public\desktop\Google Chrome.lnk':
-  #   ensure	=> absent,
-  #   subscribe	=> Package['GoogleChrome'],
-  # }
-
-  package { 'Firefox':
-    ensure      => latest,
-    provider    => $provider,
-    # require	=> File['C:\users\public\desktop\Google Chrome.lnk'],
-  }
-
-  file { 'C:\users\public\desktop\Mozilla Firefox.lnk':
-    ensure	=> absent,
-    subscribe	=> Package['Firefox'],
-  }
-
-  # package { 'flashplayeractivex':
-  #   ensure      => installed,
-  #   provider    => $provider,
-  #   require	=> File['C:\users\public\desktop\Mozilla Firefox.lnk'],
-  # }
-
-  # package { 'flashplayerplugin':
-  #   ensure      => latest,
-  #   provider    => $provider,
-  #   require	=> Package['flashplayeractivex'],
-  # }
-
-  package { 'adobereader':
-    ensure      => latest,
-    provider    => $provider,
-    # require	=> Package['flashplayerplugin'],
-  }
-
-  file { 'C:\users\public\desktop\Adobe Reader DC.lnk':
-    ensure	=> absent,
-    subscribe	=> Package['adobereader'],
-  }
-
-  # package { 'javaruntime':
-  #   ensure      => latest,
-  #   provider    => $provider,
-  #   require	=> File['C:\users\public\desktop\Adobe Reader XI.lnk'],
-  # }
-
-  package { '7zip':
-    ensure      => latest,
-    provider    => $provider,
-    # require	=> Package['javaruntime'],
-  }
-
-  # package { 'iTunes':
-  #   ensure      => latest,
-  #   provider    => $provider,
-  #   require	=> Package['7zip'],
-  # }
 
 }
